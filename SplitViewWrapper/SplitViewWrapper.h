133f7686b3e13f55aaeb9f30c4e62075fca6d102
Rahil Patel <rahil.patel@catconsult.com>
2019-06-19 20:26:18 -0500

    Initial Commit

---
+++ SplitViewWrapper/SplitViewWrapper.h
@@ -0,0 +1,19 @@
+//
+//  SplitViewWrapper.h
+//  SplitViewWrapper
+//
+//  Created by Rahil Patel on 6/19/19.
+//  Copyright © 2019 RahilPatel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SplitViewWrapper.
+FOUNDATION_EXPORT double SplitViewWrapperVersionNumber;
+
+//! Project version string for SplitViewWrapper.
+FOUNDATION_EXPORT const unsigned char SplitViewWrapperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SplitViewWrapper/PublicHeader.h>
+
+
