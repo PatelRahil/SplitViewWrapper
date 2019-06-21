3473cb15119a94bb9b68f2355c275a107bd847e9
Rahil Patel <rahil.patel@catconsult.com>
2019-06-21 17:27:14 -0400

    added a check to make sure detail view's data type is the same as the rest of the models' data type

--- SplitViewWrapper/Models/SplitViewWrapperData.swift
+++ SplitViewWrapper/Models/SplitViewWrapperData.swift
@@ -12,7 +12,7 @@ public class SplitViewWrapperData<DataSource: SplitViewDataSource, ListViewItem:
     var dataSource: DataSource
     var searchableSplitViewDataModel: SearchableSplitViewDataModel<DataSource.DataType>
     var splitViewModel: SplitViewModel<ListViewItem, DetailViewItem>
-    public init<T: SplitViewDataSource, U: SplitViewListItemProtocol, V: SplitViewDetailProtocol>(dataSource: T, searchableSplitViewDataModel: SearchableSplitViewDataModel<T.DataType>, splitViewModel: SplitViewModel<U,V>) where T.DataType == U.DataType {
+    public init<T: SplitViewDataSource, U: SplitViewListItemProtocol, V: SplitViewDetailProtocol>(dataSource: T, searchableSplitViewDataModel: SearchableSplitViewDataModel<T.DataType>, splitViewModel: SplitViewModel<U,V>) where T.DataType == U.DataType, U.DataType == V.DataType {
         self.dataSource = dataSource as! DataSource
         self.searchableSplitViewDataModel = searchableSplitViewDataModel as! SearchableSplitViewDataModel<DataSource.DataType>
         self.splitViewModel = splitViewModel as! SplitViewModel<ListViewItem, DetailViewItem>
